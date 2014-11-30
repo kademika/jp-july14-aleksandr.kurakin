39259764fc0b2b9482869daf6a49318c5ad8372c
Aleksandr <sobbako@gmail.com>
2014-11-30 18:36:28 +0300

    Tanks making UI 30.11.2014 18:31

--- src/com/kademika/tanksGame/MainFrame.java
+++ src/com/kademika/tanksGame/MainFrame.java
@@ -9,33 +9,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class MainFrame extends JFrame{
-BattleField battleField;
-ActionField af;
+public class MainFrame extends JFrame {
+    BattleField battleField;
+    ActionField af;
+    JFrame frame = new JFrame("Tanks");
 
-
-    public  MainFrame(ActionField af){
+    public MainFrame(ActionField af) {
 
         this.af = af;
         battleField = af.getBattleField();
 
-        JFrame frame = new JFrame("Tanks");
+
         frame.setLocation(650, 150);
         frame.setMinimumSize(new Dimension(battleField.getBfWidth(), battleField.getBfHeight() + 22));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.getContentPane().add(startPanel(this));
+        frame.getContentPane().add(startPanel());
         frame.pack();
         frame.setVisible(true);
     }
 
-    public JPanel startPanel(final MainFrame frame){
+    public JPanel startPanel() {
 
 //        final JFrame frame1 = frame;
         JPanel sp = new JPanel();
         sp.setLayout(new GridBagLayout());
 
         JLabel lName = new JLabel("Choose your tank");
-        String[] items =  {"Aggressor", "Defender"};
+        String[] items = {"Aggressor", "Defender"};
         final JComboBox patternList = new JComboBox(items);
         JButton buttonStart = new JButton("Start the game");
         JButton buttonReplay = new JButton("Replay");
@@ -45,7 +45,7 @@ ActionField af;
                 0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, 0, new Insets(0, 0, 0, 0), 0, 0));
         sp.add(patternList, new GridBagConstraints(
                 0, 1, 1, 1, 0, 0, GridBagConstraints.CENTER, 0, new Insets(0, 0, 0, 0), 0, 0));
-        sp.add(buttonStart,new GridBagConstraints(
+        sp.add(buttonStart, new GridBagConstraints(
                 0, 2, 1, 1, 0, 0, GridBagConstraints.CENTER, 0, new Insets(0, 0, 0, 0), 0, 0));
         sp.add(buttonReplay, new GridBagConstraints(
                 0, 3, 1, 1, 0, 0, GridBagConstraints.CENTER, 0, new Insets(0, 0, 0, 0), 0, 0));
